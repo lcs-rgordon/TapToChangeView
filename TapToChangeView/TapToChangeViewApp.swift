ef3af7c8f1fd25c3bbea9b2794028049e718493c
Russell Gordon <rgordon@lcs.on.ca>
2023-01-16 20:47:10 -0500

    Initial Commit

---
+++ TapToChangeView/TapToChangeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapToChangeViewApp.swift
+//  TapToChangeView
+//
+//  Created by Russell Gordon on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct TapToChangeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
